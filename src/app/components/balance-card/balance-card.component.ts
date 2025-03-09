433f6dc4479f810170c5081f37d8f9e16240aae5
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2025-03-09 07:10:17 -0300

    create balance card

---
+++ src/app/components/balance-card/balance-card.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance-card',
+  standalone: false,
+  templateUrl: './balance-card.component.html',
+  styleUrl: './balance-card.component.scss',
+})
+export class BalanceCardComponent {}
